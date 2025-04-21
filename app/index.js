b10a14292bc5bf2839ec900a4271897eaab1660e
Massyl Chaker <103056826+DeeperBlue01@users.noreply.github.com>
2025-04-22 00:14:22 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('🚀 CI/CD OK');
+  res.send('CI/CD OK');
 });
 
 const PORT = process.env.PORT || 3000;
