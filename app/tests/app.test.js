08d74ed8e7b4e30cda6717d9f48eadec624b6192
Massyl Chaker <103056826+DeeperBlue01@users.noreply.github.com>
2025-04-22 00:14:36 +0200

    Update app.test.js

--- app/tests/app.test.js
+++ app/tests/app.test.js
@@ -2,12 +2,12 @@ const request = require('supertest');
 const express = require('express');
 
 const app = express();
-app.get('/', (req, res) => res.send('🚀 CI/CD OK'));
+app.get('/', (req, res) => res.send('CI/CD OK'));
 
 describe('GET /', () => {
   it('should return CI/CD OK message', async () => {
     const res = await request(app).get('/');
-    expect(res.text).toBe('🚀 CI/CD OK');
+    expect(res.text).toBe('CI/CD OK');
     expect(res.statusCode).toBe(200);
   });
 });
